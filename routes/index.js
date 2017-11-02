1e3d6e6cb3a01d1e03adb4c2837a4480f25ed682
bexredd <r.redd94@gmail.com>
2017-11-02 03:39:10 +0000

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.sendFile('/index.html',{root:'public'}); 
+ //res.render('index', { title: 'Express' });
+});
+
+router.get("/nasaImage", function(req,res,next){
+	console.log("in Nasa function");
+	request("https://images-api.nasa.gov/search?q="+req.query.q).pipe(res);
 });
 
 module.exports = router;
